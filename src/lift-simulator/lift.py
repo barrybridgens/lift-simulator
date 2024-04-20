ae6d7fdd9997c5b78c973218243aee2561212f01
Barry Bridgens <barry@bridgens.me.uk>
2024-04-20 15:39:51 +0100

    Now stops at intermediate requested floors Door opening / closing logic implemented

--- src/lift-simulator/lift.py
+++ src/lift-simulator/lift.py
@@ -11,6 +11,8 @@ class Lift:
         self.inter_floor_position = 0
         self.inter_floor_steps = 10
         self.doors_open = False
+        self.doors_open_timer = 0
+        self.doors_open_time = 5
 
     def floor_button_pressed(self, floor_button):
         if self.target_floors.count(floor_button) == 0:
@@ -25,8 +27,9 @@ class Lift:
 
     def time_step(self):
         if self.is_moving():
-            # Moving - update lift position
-            self.inter_floor_position = self.inter_floor_position + 1
+            if not self.doors_open:
+                # Moving - update lift position
+                self.inter_floor_position = self.inter_floor_position + 1
             if self.inter_floor_position >= self.inter_floor_steps:
                 # Next floor reached
                 self.inter_floor_position = 0
@@ -34,11 +37,17 @@ class Lift:
                     self.floor = self.floor + 1
                 if self.moving_down:
                     self.floor = self.floor - 1
+            # Check if we have reach the original target floor
             if self.floor == self.target_floors[0]:
                 # Arrived at the target floor
                 self.moving_up = False
                 self.moving_down = False
                 dummy = self.target_floors.pop(0)
+                self.doors_open = True
+            # Check if we have reached a floor that has been requested
+            if self.target_floors.count(self.floor) > 0:
+                self.target_floors.remove(self.floor)
+                self.doors_open = True
         else:
             # Not moving - check for button preses
             if len(self.target_floors) != 0:
@@ -46,3 +55,10 @@ class Lift:
                     self.moving_up = True
                 else:
                     self.moving_down = True
+        # Check door status
+        if self.doors_open:
+            # Update door status
+            self.doors_open_timer = self.doors_open_timer + 1
+            if self.doors_open_timer >= self.doors_open_time:
+                self.doors_open_timer = 0;
+                self.doors_open = False
--- src/lift-simulator/lift_simulator.py
+++ src/lift-simulator/lift_simulator.py
@@ -6,7 +6,7 @@ from lift import Lift
 test_lift = Lift(8)
 
 
-test_lift.floor_button_pressed(7)
+test_lift.floor_button_pressed(5)
 test_lift.floor_button_pressed(2)
 
 while (True):
@@ -15,6 +15,8 @@ while (True):
     print(test_lift.is_moving())
     print(test_lift.inter_floor_position)
     print(test_lift.target_floors)
+    if test_lift.doors_open:
+        print("Doors Open")
     print("---------------------------------------")
     time.sleep(1)
 
